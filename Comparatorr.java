548ae90c1c93492024129b96c92e931fbd85fb6b
manasijadhav1 <manasijadhav2410@gmail.com>
2025-12-10 11:11:08 +0530

    Comparator

---
+++ Comparatorr.java
@@ -0,0 +1,48 @@
+import java.util.*;
+class Comp implements Comparator<Integer>
+{
+   public int compare(Integer o1,Integer o2)
+    {
+        if(o1%10>o2%10)
+        {
+            return 1;
+        }
+        return -1;
+    }
+
+
+}
+public class Comparatorr {
+    public static void main(String[] args) {
+        List<Integer> values=new ArrayList<>();
+        values.add(109);
+        values.add(453);
+        values.add(501);
+        values.add(908);
+        values.add(302);
+
+       Comparator<Integer> a=new Comp();
+
+
+        //Using Ananomous class
+        /*Comparator<Integer> a=new Comparator<Integer>()
+        {
+            public int compare(Integer o1,Integer o2)
+            {
+                if(o1%10>o2%10)
+                {
+                    return 1;
+                }
+                return -1;
+            }
+        };*/
+        Collections.sort(values,a);
+
+        Iterator i=values.iterator();
+        while(i.hasNext())
+        {
+            System.out.println(i.next());
+        }
+
+    }
+}
